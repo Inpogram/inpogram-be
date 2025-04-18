3dc9d479fb84206a8b437ddb26b6b06b131520dc
haibt1112 <haibui.cs@gmail.com>
2025-04-18 13:10:05 +0700

    feature/unit-tests-ci: fix failed to load context

--- src/test/java/com/haibui/inpogram/InpogramApplicationTests.java
+++ src/test/java/com/haibui/inpogram/InpogramApplicationTests.java
@@ -1,12 +1,9 @@
 package com.haibui.inpogram;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude=SecurityAutoConfiguration.class)
 class InpogramApplicationTests {
 
     @Test
